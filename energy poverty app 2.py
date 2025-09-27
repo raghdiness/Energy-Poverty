ee4497569b528acf32c4b55af3c463fe4c5489a1
Rae Heikel <102865269+raghdiness@users.noreply.github.com>
2025-09-27 16:31:10 +0300

    Update energy poverty app 2.py

--- energy poverty app 2.py
+++ energy poverty app 2.py
@@ -48,20 +48,23 @@ fuel_url = st.text_input("Fuel poverty CSV URL or path")
 
 if fuel_url:
     fuel_df = load_and_clean_fuel_data(fuel_url)
+merged = fuel_df.copy()
+merged["label"] = (
+    merged["Proportion of households fuel poor (%)"] >= 10
+).astype(int)
 
+drop_cols = [
+    "LSOA Code",
+    "LSOA Name",
+    "Local Authority Code",
+    "Local Authority Name",
+    "Region",
+    "label"
+]
 
-        # --- Select only numeric columns for features ---
-    drop_cols = [
-            "LSOA Code",
-            "LSOA Name",
-            "Local Authority Code",
-            "Local Authority Name",
-            "Region",
-            "label"
-        ]
-    X = fuel_df.drop(columns=drop_cols, errors="ignore").select_dtypes(include=["number"])
-    y = fuel_df["label"].values
-    meta = fuel_df[["LSOA Code", "LSOA Name"]]
+X = merged.drop(columns=drop_cols, errors="ignore").select_dtypes(include=["number"])
+y = merged["label"].values
+meta = merged[["LSOA Code", "LSOA Name"]]
 
     st.write("Feature dtypes:", X.dtypes)
 
